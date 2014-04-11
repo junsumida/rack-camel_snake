a0097389993148b763e43022f028c4fe6e691986
junsumida <jun.sumida@mixi.co.jp>
2014-04-11 17:57:37 +0900

    use expect instead of should

--- spec/rack/camel_snake/refinements_spec.rb
+++ spec/rack/camel_snake/refinements_spec.rb
@@ -18,16 +18,16 @@ describe Rack::CamelSnake::Refinements do
   describe 'to_camel' do
     it 'convert snake_case into camelCase' do
       %w(_camel_case camel_case camel___case camel_case_).each do |word|
-        mocked.to_camel(word).should eq 'camelCase'
+        expect(mocked.to_camel(word)).to eq 'camelCase'
       end
     end
   end
 
   describe 'to_snake' do
     it 'convert camelCase into snake_case' do
-      mocked.to_snake('snakeCase').should eq 'snake_case'
-      mocked.to_snake('SnakeCase').should eq 'snake_case'
-      mocked.to_snake('SNAKECase').should eq 'snake_case'
+      expect(mocked.to_snake('snakeCase')).to eq 'snake_case'
+      expect(mocked.to_snake('SnakeCase')).to eq 'snake_case'
+      expect(mocked.to_snake('SNAKECase')).to eq 'snake_case'
     end
   end
 end
